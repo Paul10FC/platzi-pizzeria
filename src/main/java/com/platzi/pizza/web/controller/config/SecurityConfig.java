e7afa7350d0e2538a185d8af70ce75d900e0cd97
Paul Flores <paulalanflores@gmail.com>
2023-10-20 21:32:19 -0600

    add the requestMatchers to the config

--- src/main/java/com/platzi/pizza/web/controller/config/SecurityConfig.java
+++ src/main/java/com/platzi/pizza/web/controller/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.platzi.pizza.web.controller.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,9 +16,16 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(customizeRequest -> {
                     customizeRequest
+                            .requestMatchers(HttpMethod.PUT)
+                            .denyAll()
+                            .requestMatchers(HttpMethod.GET, "/api/**")
+                            .permitAll()
                             .anyRequest()
                             .authenticated();
-                }).httpBasic(Customizer.withDefaults());
+                })
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 }
