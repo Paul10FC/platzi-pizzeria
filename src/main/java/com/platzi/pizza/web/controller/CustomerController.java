30358ea3e5f5ca1404e87559e0b4de4be3edc9f7
Paul Flores <paulalanflores@gmail.com>
2023-10-23 18:37:03 -0600

    add the customer orders RequestMapping and the dependence inyection of the orderService

--- src/main/java/com/platzi/pizza/web/controller/CustomerController.java
+++ src/main/java/com/platzi/pizza/web/controller/CustomerController.java
@@ -1,7 +1,9 @@
 package com.platzi.pizza.web.controller;
 
 import com.platzi.pizza.persistence.entity.CustomerEntity;
+import com.platzi.pizza.persistence.entity.OrderEntity;
 import com.platzi.pizza.service.CustomerService;
+import com.platzi.pizza.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,19 +11,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/customer")
 public class CustomerController {
 
     private final CustomerService customerService;
+    private final OrderService orderService;
 
     @Autowired
-    public CustomerController(CustomerService customerService) {
+    public CustomerController(CustomerService customerService, OrderService orderService) {
         this.customerService = customerService;
+        this.orderService = orderService;
     }
 
     @GetMapping("/phone/{phoneNumber}")
     public ResponseEntity<CustomerEntity> getByPhone(@PathVariable String phoneNumber){
         return ResponseEntity.ok(customerService.findByPhone(phoneNumber));
+    }
+
+    @RequestMapping("/customer/{id}")
+    public ResponseEntity<List<OrderEntity>> getCustomerOrders(@PathVariable String id){
+        return ResponseEntity.ok(this.orderService.getByCustomerId(id));
     }
 }
