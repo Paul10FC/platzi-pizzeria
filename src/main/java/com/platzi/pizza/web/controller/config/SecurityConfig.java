7caee099971b52f8a849db62182a59d14c57244f
Paul Flores <paulalanflores@gmail.com>
2023-10-23 18:38:09 -0600

    Enable the method security with the securedEnable

--- src/main/java/com/platzi/pizza/web/controller/config/SecurityConfig.java
+++ src/main/java/com/platzi/pizza/web/controller/config/SecurityConfig.java
@@ -2,19 +2,19 @@ package com.platzi.pizza.web.controller.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
     @Bean
@@ -22,10 +22,11 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(customizeRequest -> {
                     customizeRequest
-                            .requestMatchers(HttpMethod.PUT).hasRole("ADMIN")
+                            .requestMatchers("/api/customer/**").hasAnyRole("ADMIN", "CUSTOMER")
+                            .requestMatchers(HttpMethod.PUT).hasAnyRole("ADMIN", "CHEF")
                             .requestMatchers(HttpMethod.GET, "/api/pizzas/**").hasAnyRole("ADMIN", "CUSTOMER")
-                            .requestMatchers(HttpMethod.POST).hasRole("ADMIN")
-                            .requestMatchers("/api/order/**").hasRole("ADMIN")
+                            .requestMatchers("/api/order/random").hasAuthority("random_order")
+                            .requestMatchers("/api/order/**").hasAnyRole("ADMIN", "CHEF")
                             .anyRequest()
                             .authenticated();
                 })
@@ -33,23 +34,6 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
-    }
-
-    @Bean
-    public UserDetailsService memoryUser(){
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .build();
-
-        UserDetails customer = User.builder()
-                .username("customer")
-                .password(passwordEncoder().encode("customer123"))
-                .roles("CUSTOMER")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, customer);
     }
 
     @Bean
