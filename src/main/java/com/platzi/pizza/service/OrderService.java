acb08558269bcf90a780f7d59f360261b332bf5b
Paul Flores <paulalanflores@gmail.com>
2023-10-23 18:35:27 -0600

    add the @Secured annotation for the admin role

--- src/main/java/com/platzi/pizza/service/OrderService.java
+++ src/main/java/com/platzi/pizza/service/OrderService.java
@@ -5,6 +5,7 @@ import com.platzi.pizza.persistence.projection.OrderSummary;
 import com.platzi.pizza.persistence.repository.OrderRepository;
 import com.platzi.pizza.service.dto.RandomOrderDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -43,6 +44,7 @@ public class OrderService {
         return this.orderRepository.findAllByMethodIn(methods);
     }
 
+    @Secured("ROLE_ADMIN")
     public List<OrderEntity> getByCustomerId(String id){
         return this.orderRepository.findCustomerOrders(id);
     }
