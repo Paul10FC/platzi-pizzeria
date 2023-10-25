e959bc0025f6fd20874a16d06706d348aabd0d2a
Paul Flores <paulalanflores@gmail.com>
2023-10-24 22:50:03 -0600

    add the listeners for the pizzas creation and modifiers

--- src/main/java/com/platzi/pizza/persistence/audit/AuditableEntity.java
+++ src/main/java/com/platzi/pizza/persistence/audit/AuditableEntity.java
@@ -2,7 +2,9 @@ package com.platzi.pizza.persistence.audit;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
@@ -17,4 +19,12 @@ public class AuditableEntity {
     @Column(name = "modified_date")
     @LastModifiedDate
     private LocalDateTime modifiedDate;
+
+    @Column(name = "created_by")
+    @CreatedBy
+    private String modifiedBy;
+
+    @LastModifiedBy
+    @Column(name = "modified_by")
+    private String createdBy;
 }
