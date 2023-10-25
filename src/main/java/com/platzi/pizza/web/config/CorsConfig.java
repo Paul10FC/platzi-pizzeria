33ea7cd5e3ea426ab500e55f82447de90c3085eb
Paul Flores <paulalanflores@gmail.com>
2023-10-24 22:24:24 -0600

    delete one innecesary line

--- src/main/java/com/platzi/pizza/web/config/CorsConfig.java
+++ src/main/java/com/platzi/pizza/web/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.platzi.pizza.web.controller.config;
+package com.platzi.pizza.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
